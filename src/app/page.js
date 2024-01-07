ce02d12341230de11c65b3d61c2385eabbe85287
SangWon7242 <gotkdqja@gmail.com>
2024-01-07 14:50:39 +0900

    머터리얼 UI 설치

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,27 @@
-import Image from 'next/image'
+"use client"
 
-export default function Home() {
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {    
+
   return (
-    <main>
-      <div>
-        환영합니다.
-      </div>
-    </main>
-  )
+    <>
+      <Head>        
+        <meta charset="UTF-8" />	            
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>  
+      <h1>앱 소개 페이지</h1>
+
+      <nav>
+        <Link href="/" legacyBehavior>
+          <a className="text-red-300">메인</a>
+        </Link>
+
+        <Link href="/about" legacyBehavior>
+          <a className="text-blue-300">어바웃</a>
+        </Link>
+      </nav> 
+    </>  
+  );
 }
