77a087a28ce9be6b7158a101baeff20833d574fb
SangWon7242 <gotkdqja@gmail.com>
2024-01-13 12:36:00 +0900

    프리티어 세팅

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
-import './globals.css'
+import './globals.css';
 
 export const metadata = {
   title: '메인 | 투두',
   description: '할일관리서비스입니다.',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body> {children} </body>
     </html>
-  )
+  );
 }
