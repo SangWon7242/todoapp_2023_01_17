f6609722ba572cd62b0ec4aa9b65036fa0acc61f
SangWon7242 <gotkdqja@gmail.com>
2024-01-13 14:47:34 +0900

    snackbar, alert

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,21 @@
 'use client';
 
 import { ThemeProvider } from '@mui/material/styles';
-import { Button, Box, AppBar, Toolbar } from '@mui/material';
+import * as React from 'react';
+import { Button, Box, AppBar, Toolbar, Snackbar, Alert as MuiAlert } from '@mui/material';
 import { FaBars } from 'react-icons/fa';
 import theme from './theme';
 
+const Alert = React.forwardRef((props, ref) => {
+  return <MuiAlert {...props} ref={ref} variant="filled" />;
+});
+
 export default function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const alertRef = React.useRef(null);
+  // ref는 컴포넌트에 전달되지 않는다.
+
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -25,10 +35,20 @@ export default function App() {
           </Toolbar>
         </AppBar>
         <Toolbar />
-        <section className="tw-h-screen tw-flex tw-items-center tw-justify-center tw-text-[5rem]">
-          섹션
-        </section>
       </ThemeProvider>
+      <section>
+        <Button onClick={() => setOpen(true)} variant="contained">
+          게시물 삭제
+        </Button>
+        <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+          {/* <Alert ref={alertRef} severity="success">
+            게시물이 삭제되었습니다.
+          </Alert> */}
+          <Alert ref={alertRef} severity="warning">
+            게시물이 삭제되었습니다.
+          </Alert>
+        </Snackbar>
+      </section>
     </>
   );
 }
